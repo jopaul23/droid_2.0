f437bab6815866c938092c85dda26264e7b7661e
Joyal Raphel <joyalraphel00@gmail.com>
2021-12-05 04:29:49 +0530

    qrlog added

---
+++ lib/screens/scanningPage/scannerlog.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ScannerLog extends StatelessWidget {
+  const ScannerLog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Card(
+            child:Column(children: [
+              
+            ],)
+          ),
+        ),
+      ),
+    );
+  }
+}
